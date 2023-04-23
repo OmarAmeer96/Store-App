3653472f2ffaf356a3023d9afbb2538bcc47dcdf
Omar_Ameer <omar.ameer244@gmail.com>
2023-04-23 12:18:47 +0200

    5th Commit

--- lib/services/get_all_products_service.dart
+++ lib/services/get_all_products_service.dart
@@ -7,13 +7,18 @@ class GetAllProductsService {
     http.Response response =
         await http.get(Uri.parse('https://fakestoreapi.com/products'));
 
-    List<dynamic> data = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
 
-    List<ProductModel> productsList = [];
-    for (int i = 0; i < data.length; i++) {
-      productsList.add(ProductModel.fromJson(data[i]));
+      List<ProductModel> productsList = [];
+      for (int i = 0; i < data.length; i++) {
+        productsList.add(ProductModel.fromJson(data[i]));
+      }
+
+      return productsList;
+    } else {
+      throw Exception(
+          "There's a problem in the status code!, ${response.statusCode}");
     }
-
-    return productsList;
   }
 }
