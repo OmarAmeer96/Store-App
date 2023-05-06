8a64badc829b1116c75b59e06038e0d78ae35609
Omar_Ameer <omar.ameer244@gmail.com>
2023-05-06 12:34:56 +0300

    15th Commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -32,57 +32,73 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Container(
-          height: 150,
-          width: 190,
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 40,
-                color: Colors.grey.withOpacity(0.2),
-                spreadRadius: 0,
-                offset: const Offset(10, 10),
-              ),
-            ],
-          ),
-          child: Card(
-            elevation: 8,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'HandBag LV',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 18,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 6,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        r'$315',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                        ),
-                      ),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.red,
-                      ),
-                    ],
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              height: 135,
+              width: 200,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 40,
+                    color: Colors.grey.withOpacity(0.1),
+                    spreadRadius: 0,
+                    offset: const Offset(10, 10),
                   ),
                 ],
               ),
+              child: Card(
+                elevation: 8,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 14,
+                    vertical: 14,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'HandBag LV',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text(
+                            r'$315',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ),
+            Positioned(
+              right: 23,
+              top: -60,
+              child: Image.network(
+                'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+                height: 110,
+              ),
+            ),
+          ],
         ),
       ),
     );
